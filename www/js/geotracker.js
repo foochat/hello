cc3921deb9cff05ee08ae803542bc02021a5a8ec
foochat <antoninpuleo@gmail.com>
2015-01-12 13:48:23 +0100

    trying to fix bug

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -66,8 +66,8 @@ $("#startTracking_stop").live('click', function(){
 	window.localStorage.setItem(track_id, JSON.stringify(tracking_data));
 
 	// Reset watch_id and tracking_data 
-	var watch_id = null;
-	var tracking_data = null;
+	watch_id = null;
+	tracking_data = [];
 
 	// Tidy up the UI
 	$("#track_id").val("").show();
