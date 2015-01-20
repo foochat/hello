2ce0440b61e61467ff4352a6bd7910405038a0ee
foochat <antoninpuleo@gmail.com>
2015-01-20 16:53:22 +0100

    test with local icecast2 server

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -289,7 +289,8 @@ $("#home_radio_button").live('click', function(){
 	try {
 		if(myaudio == null)
 		{
-			myaudio = new Audio('http://streaming.rtbf.be:8000/2128xrtbf');
+			//myaudio = new Audio('http://streaming.rtbf.be:8000/2128xrtbf');
+			myaudio = new Audio('http://10.1.10.205:8000/stream.ogg');
 			myaudio.id = 'playerAudio';
 			myaudio.play();
 		} 
