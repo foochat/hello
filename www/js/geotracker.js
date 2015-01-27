8455313b0e5b1843c9e10543f8b7efcc397f09a3
Antonin Puleo <antoninpuleo@gmail.com>
2015-01-27 14:49:20 +0100

    i don't like webkit audio

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -36,7 +36,7 @@ window.addEventListener('load', init, false);
 function init() {
     try {
         // Fix up for prefixing
-        window.AudioContext = window.AudioContext||window.webkitAudioContext;
+        //window.AudioContext = window.AudioContext||window.webkitAudioContext;
         context = new AudioContext();
     } catch(e) {
         alert('Web Audio API is not supported in this browser');
