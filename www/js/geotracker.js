73629adc901e048e2dc96e2b8033948193ec1978
Antonin Puleo <antoninpuleo@gmail.com>
2015-01-27 11:10:17 +0100

    Web audio API not working..

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -35,7 +35,9 @@ var context = null;
 window.addEventListener('load', init, false);
 function init() {
     try {
-        context = new webkitAudioContext();
+        // Fix up for prefixing
+        window.AudioContext = window.AudioContext||window.webkitAudioContext;
+        context = new AudioContext();
     } catch(e) {
         alert('Web Audio API is not supported in this browser');
     }
