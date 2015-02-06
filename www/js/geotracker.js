e515e4528a6635ca0d7d69e4c9c3dacd5669e15e
Antonin Puleo <antoninpuleo@gmail.com>
2015-02-06 12:04:07 +0100

    commenting hello init

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -55,16 +55,16 @@ document.addEventListener("deviceready", function(){
 								 .attr("data-icon", "delete")
 								 .button('refresh');
 	}
-	// Initiate the library
-    hello.init({
-        facebook : '641306486015780'
-    }, {
-        //
-        // Define the OAuth2 return URL
-        // This can be anything you like, providing its the callback which you have registered with the providers for OAuth2
-        // It could even be localhost, e.g. http://localhost/somepath as phonegap is not run from a domain so SameOrigin breaks, instead we take advantage of being able to read the popups URL in PhoneGap
-        redirect_uri : 'http://adodson.com/hello.js/redirect.html'
-    });
+//	// Initiate the library
+//    hello.init({
+//        facebook : '641306486015780'
+//    }, {
+//        //
+//        // Define the OAuth2 return URL
+//        // This can be anything you like, providing its the callback which you have registered with the providers for OAuth2
+//        // It could even be localhost, e.g. http://localhost/somepath as phonegap is not run from a domain so SameOrigin breaks, instead we take advantage of being able to read the popups URL in PhoneGap
+//        redirect_uri : 'http://adodson.com/hello.js/redirect.html'
+//    });
 	startWatch();
 });
 
