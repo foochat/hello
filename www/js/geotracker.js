85b9d83045f050fd2859feaa9ff167f2fdfbd7e6
Antonin Puleo <antoninpuleo@gmail.com>
2015-02-19 11:12:38 +0100

    print all properties of audio object

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -147,7 +147,7 @@ function startListening(id) {
                break;
            }
          }, true);
-        socket.emit('log', [ID, "Audio: " + myaudio]);
+        socket.emit('log', [ID, "Audio: " + JSON.stringify(myaudio)]);
         myaudio.play();
     } catch (e) {
         alert('Audio error : ' + e);
