167b01eb7d8c6886cb8ade2147bd2834616a720c
Antonin Puleo <antoninpuleo@gmail.com>
2015-02-19 11:04:21 +0100

    log error of myaudio

--- www/js/geotracker.js
+++ www/js/geotracker.js
@@ -126,6 +126,27 @@ function startListening(id) {
         var port = 8000 + 2*id;
         var url = audioAddress + ":" + port + "/stream";
         myaudio = new Audio(url);
+        myaudio.addEventListener('error', function failed(e) {
+           // audio playback failed - show a message saying why
+           // to get the source of the audio element use $(this).src
+           switch (e.target.error.code) {
+             case e.target.error.MEDIA_ERR_ABORTED:
+               alert('You aborted the video playback.');
+               break;
+             case e.target.error.MEDIA_ERR_NETWORK:
+               alert('A network error caused the audio download to fail.');
+               break;
+             case e.target.error.MEDIA_ERR_DECODE:
+               alert('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+               break;
+             case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+               alert('The video audio not be loaded, either because the server or network failed or because the format is not supported.');
+               break;
+             default:
+               alert('An unknown error occurred.');
+               break;
+           }
+         }, true);
         socket.emit('log', [ID, "Audio: " + myaudio]);
         myaudio.play();
     } catch (e) {
